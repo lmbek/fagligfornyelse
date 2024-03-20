56bceeb724c1a7567cd159cf8e956deaf0cd72d7
Lars Morten Bek <lars@beksoft.dk>
2024-03-20 21:52:06 +0100

    Refactor esbuild.js and remove redundant guide
    This commit removes the obsolete 'esbuild-go-react-guide.md' file and extensively refactors 'esbuild.js'. Changes to 'esbuild.js' include adding new builds for 'main.min.css', 'main.bundle.min.js', and 'admin.bundle.min.js', while ensuring the use of the 'es2020' target for all builds, for optimization.

--- esbuild.js
+++ esbuild.js
@@ -1,18 +1,48 @@
 const esbuild = require("esbuild");
 
+// main.min.css
 esbuild.build({
-	entryPoints: ['./frontend/dev/js/admin/app.jsx','./frontend/dev/js/web/htmx.js', './frontend/dev/js/dev/autorefresher.js'],
+	entryPoints: ['./frontend/src/css/main.css'],
 	sourcemap: false,
 	bundle: true,
-	outdir: './frontend/live/public/js/',
-	target: 'es2016',
+	outfile: './frontend/out/css/main.min.css', // Corrected file extension
+	minify: true,
+	target: 'es2020', // This is fine if you're targeting specific CSS features
+}).then(() => {
+	console.log("main.min.css build complete!");
+}).catch(() => {
+	process.exit(1);
+});
+
+// main.bundle.min.js
+esbuild.build({
+	entryPoints: ['./frontend/src/js/main.js'],
+	sourcemap: false,
+	bundle: true,
+	outfile: './frontend/out/js/main.bundle.min.js',
+	target: 'es2020', /* es2016 */
+	minify: true,
+	platform: 'browser',
+}).then(()=>{
+	console.log("main.bundle.min.js build complete!")
+}).catch(()=>{
+	process.exit(1)
+});
+
+// admin.bundle.min.js
+esbuild.build({
+	entryPoints: ['./frontend/src/js/admin/app.jsx'],
+	sourcemap: false,
+	bundle: true,
+	outfile: './frontend/out/js/admin.bundle.min.js',
+	target: 'es2020',
 	jsxFactory: 'React.createElement',
 	jsxFragment: 'React.Fragment',
 	minify: true,
 	plugins: [],
 	platform: 'browser',
 }).then(()=>{
-	console.log("Build Complete!")
+	console.log("admin.bundle.min.js build complete!")
 }).catch(()=>{
 	process.exit(1)
 });
